82ce4391539b2ead444993a7a1480ae19516878a
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-08-15 16:49:07 +0300

    tesisatizmirde.com | about section modifed.

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -7,16 +7,16 @@ const About = () => {
 
   const { ref: titleRef, inView: isTitleVisible } = useInView()
   const { ref: textRef, inView: isTextVisible } = useInView()
+  const { ref: imgRef, inView: isImageVisible } = useInView()
 
   return (
     <section className={styles.about}>
       <article className={styles.about_text}>
         <h2 className={`${styles.title} ${styles.hidden} ${isTitleVisible && styles.visible}`} ref={titleRef}>Biz <span>Kimiz?</span></h2>
         <p ref={textRef} className={`${styles.text} ${styles.hidden} ${isTextVisible && styles.visible}`} >Sektörde geçen on yılı aşkın süre boyunca, binlerce başarılı projeye imza attık ve müşterilerimize en yüksek kalitede hizmet sunma konusundaki taahhüdümüzü sürdürdük. Alanında uzman ekip arkadaşlarımızla bir araya gelerek, her türlü sıhhi tesisat ihtiyacınızı karşılamak için donanımlıyız.</p>
-        <button>Daha Fazlası...</button>
       </article>
       <article className={styles.about_image}>
-        <img src={hero} alt="" />
+        <img className={`${styles.hidden_img} ${isImageVisible && styles.visible_img}`} src={hero} alt="" ref={imgRef} />
       </article>
     </section>
   )
